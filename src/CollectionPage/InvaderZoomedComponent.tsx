4f3773807e22256aede04107439fb8204e0577c2
Nicolas Marlier <nmarlier@gmail.com>
2024-05-12 20:06:47 +0200

    ZoomedInvaderStyling

--- src/CollectionPage/InvaderZoomedComponent.tsx
+++ src/CollectionPage/InvaderZoomedComponent.tsx
@@ -20,19 +20,41 @@ const InvaderZoomedComponent = (props: Props) => {
         setClassNames([])
         setTimeout(onClose, 300)
     }
+    const capitalize = (str: string) =>  str.charAt(0).toUpperCase() + str.slice(1)
+
     return <div onClick={attemptClose} className={`invader-zoomed ${classNames.join(' ')}`}>
         <div className="flex-space"/>
         <img width="1000" height="1000" src={image}/>
         <div className="flex-space"/>
         <div className="details">
-            <div className="name">{ name }</div>
-            <div className="score">{ point }</div>
-            
-            
-            <div className="dates">
-                { moment(date_pos).format("YYYY") } • flashé { moment(date_flash).locale("fr").format('dddd D MMMM YYYY HH:mm') }
+            <div className="line">
+                <label>Nom</label>
+                <div className="value-container">
+                    . . . . . . . . . . . . . . . .
+                    <div className="value">{ name }</div>
+                </div>
             </div>
-            
+            <div className="line">
+                <label>Points</label>
+                <div className="value-container">
+                    . . . . . . . . . . . . . . . .
+                    <div className="value">{ point }</div>
+                </div>
+            </div>
+            <div className="line">
+                <label>Posé</label>
+                <div className="value-container">
+                    . . . . . . . . . . . . . . . .
+                    <div className="value">{ moment(date_pos).format("YYYY") }</div>
+                </div>
+            </div>
+            <div className="line">
+                <label>Flashé</label>
+                <div className="value-container">
+                    . . . . . . . . . . . . . . . .
+                    <div className="value value-small">{ capitalize(moment(date_flash).locale("fr").format('dddd DD/MM/YY, HH:mm')) }</div>
+                </div>
+            </div>        
         </div>
     </div>
 }
